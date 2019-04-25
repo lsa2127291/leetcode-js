18b9aba3c4a5708dd7a41f0e521e3c941fe5f2cb
lsa212791 <lsa212791@gmail.com>
2019-04-25 20:27:54 +0800

    暂存

---
+++ src/limitLess.js
@@ -0,0 +1,16 @@
+ 
+function add(...args) {
+  let sum = args.reduce((acc, cur) => acc + cur);
+  const tmp = function (...args) {
+    sum = sum + args.reduce((acc, cur) => acc + cur);
+    return tmp;
+  };
+  tmp.toString = function () {
+    return sum;
+  };
+  tmp.valueOf = function () {
+    return sum;
+  };
+  return tmp;
+}
+console.log(add(1, 2)(2, 3)(4, 5) + 'fef')
